072510fb2c4385bf98e51a9ed553359cf94b1c0b
hamidkrml <karimlihamid4@gmail.com>
2025-03-18 11:02:01 +0300

    first commit

---
+++ MVVM_UiKitApp/Core/Home Screen/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  MVVM_UiKitApp
+//
+//  Created by hamid on 18.03.25.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
