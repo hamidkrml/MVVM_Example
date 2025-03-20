1db63c3fb174c27ff1cc7a3965d1a4125f15b10a
hamidkrml <karimlihamid4@gmail.com>
2025-03-20 18:57:51 +0300

    Refactor NetworkManager to add error handling in download function

--- MVVM_UiKitApp/Uitillites/NetworkManeger.swift
+++ MVVM_UiKitApp/Uitillites/NetworkManeger.swift
@@ -8,21 +8,33 @@
 import Foundation
 
 class NetworkManeger {
+    
+    
     static let shared = NetworkManeger()
     private init(){}
     
-    func download(url:URL,completion: @escaping (Result<Data, Error>) -> ()){
-        URLSession.shared.downloadTask(with: url) { data, response, error in
+    func download(url:URL,completion: @escaping (Result<Data, Error>) -> () ){
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error{
                 print(error.localizedDescription)
                 return
             }
             guard let response = response as? HTTPURLResponse,
-                response.statusCode == 200 else {return}
+                  response.statusCode == 200 else {
+                completion(.failure(URLError(.badServerResponse)))
+                return
+                
+            }
             
-            guard let data = data else {return}
-            
+            guard let data = data else {
+                
+                completion(.failure(URLError(.badURL)))
+                return
+            }
+            completion(.success(data))
         }
+        
     }
 }
 
