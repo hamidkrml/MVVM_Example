aa145134cef3aaa2344b71d0e2a039a6afa384c5
hamidkrml <karimlihamid4@gmail.com>
2025-03-31 00:41:56 +0300

    Add downloadDetail method to MovieService for fetching movie details

--- MVVM_UiKitApp/Services/MovieService.swift
+++ MVVM_UiKitApp/Services/MovieService.swift
@@ -10,7 +10,7 @@ import Foundation
 
 final class MovieService {
     func fetchPopularMovies(page: Int, completion: @escaping ([MovieResult]?) -> Void) {
-        let endpoint = EndPoint.popularMovies(page: page)
+         let endpoint = EndPoint.popularMovies(page: page)
         
         NetworkManeger.shared.request(endpoint) { [weak self] result in
             guard let self = self else { return }
@@ -24,6 +24,24 @@ final class MovieService {
             }
         }
     }
+    
+    func downloadDetail(id:Int,completion: @escaping(MovieResult?)->Void){
+        let endpoint = EndPoint.detail(id: id)
+        
+        NetworkManeger.shared.request(endpoint) { [weak self] resultDetail in
+            guard let self = self else { return }
+            
+            switch resultDetail{
+                
+            case .success(let data):
+                completion(handleWithDataDetail(data: data))
+            case .failure(let error):
+                self.handleWithError(error: error)
+            }
+            
+        }
+    }
+    
     private func handleWithError(error: Error) {
         print(error.localizedDescription)
     }
@@ -33,6 +51,17 @@ final class MovieService {
             let movie = try JSONDecoder().decode(Movie.self, from: data)
             return movie.results
         }catch{
+            return nil
+        }
+    }
+    
+    private func handleWithDataDetail(data: Data) -> MovieResult?{
+        
+        do {
+            let movieDetail = try JSONDecoder().decode(MovieResult.self, from: data)
+            return movieDetail
+        } catch {
+            print(error)
             return nil
         }
     }
